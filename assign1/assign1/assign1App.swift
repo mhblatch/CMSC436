5f98f6b5d6b61205e151a7c373354edf983d6efe
Mei Blatchford <51713786+mhblatch@users.noreply.github.com>
2023-08-04 20:54:21 -0400

    Add files via upload
    assign1

---
+++ assign1/assign1/assign1App.swift
@@ -0,0 +1,17 @@
+//
+//  assign1App.swift
+//  assign1
+//
+//  Created by Mei-An Blatchford on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct assign1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
